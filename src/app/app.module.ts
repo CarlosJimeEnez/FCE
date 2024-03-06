670384abb71cb1420163b07f07d20985bfca6845
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-03-06 10:25:26 -0600

    creacion componente menu principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { TableroComponent } from './componentes/tablero/tablero.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
