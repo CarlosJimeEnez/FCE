0051a30c533cf18f7fd9f0331b38178eb25e7089
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-03-06 11:24:13 -0600

    add animation carousel

--- src/app/componentes/carousel-mision-vision/carousel-mision-vision.component.ts
+++ src/app/componentes/carousel-mision-vision/carousel-mision-vision.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-carousel-mision-vision',
   templateUrl: './carousel-mision-vision.component.html',
-  styleUrls: ['./carousel-mision-vision.component.css']
+  styleUrls: ['./carousel-mision-vision.component.css'],
+  animations: [
+    trigger('deslizar', [
+      state('fuera', style({ transform: "translateX(-100%)"})),
+      state('dentro', style({transform: 'translateX(0%)'})),
+      transition("fuera => dentro", [
+        animate("0.5s ease-in")
+      ]),
+      transition('dentro => fuera', [
+        animate('0.5s ease-out')
+      ])
+    ])
+  ]
 })
-export class CarouselMisionVisionComponent {
+export class CarouselMisionVisionComponent implements OnInit {
+  @ViewChild('miDiv') miDiv!: ElementRef;
+  estadoAnimacion: string = 'fuera';
 
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        this.estadoAnimacion = entry.isIntersecting ? 'dentro' : 'fuera';
+      });
+    }, { threshold: [0.5] }); // Puedes ajustar el umbral según tus necesidades
+
+    observer.observe(this.el.nativeElement);
+  }
 }
