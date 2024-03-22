705c76535051184ef7570fb9eccb73467045d0aa
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-03-22 09:47:59 -0600

    add:scroll auto top

--- src/app/componentes/tablero/tablero.component.ts
+++ src/app/componentes/tablero/tablero.component.ts
@@ -1,5 +1,5 @@
 import { fromEvent, Subscription } from 'rxjs';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { CarrerasServicesService } from 'src/app/services/carreras-services.service';
 import { Carrera } from 'src/app/interfaces/carrera';
 import { Router } from '@angular/router';
@@ -10,26 +10,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./tablero.component.css']
 })
 
-export class TableroComponent implements OnInit, OnDestroy {
+export class TableroComponent implements OnInit, OnDestroy, AfterViewInit {
   carreras: Carrera[] = [];
   carrerasNoCargadas: boolean = true;
   // Servicios con guion bajo
   constructor(private _carrerasService: CarrerasServicesService, private _router: Router) {}
+  @ViewChild("navbar") navbar!: ElementRef;
   
-
   ngOnInit(): void{
-    this.getCarreras();    
+    this.getCarreras();   
   }
   
+  ngAfterViewInit(): void {
+    this.desplazarAlInicio()
+  }
+
   ngOnDestroy(): void {
   }
 
+  // Funciones
   getCarreras() {
     this._carrerasService.getCarreras().subscribe(data => {
       console.log(data);
       this.carrerasNoCargadas = false
       this.carreras = data
     }, error => this.carrerasNoCargadas = true);
+  }
+
+  desplazarAlInicio(): void {
+    this.navbar.nativeElement.scrollTop = 0;
   }
 
   verCarrera(id: number): void {
