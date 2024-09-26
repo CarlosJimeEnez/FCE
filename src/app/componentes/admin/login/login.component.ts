a1e6c478c8309460dd328e81962a8aa340b7a89a
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-09-26 08:59:56 -0600

    paswword validators

--- src/app/componentes/admin/login/login.component.ts
+++ src/app/componentes/admin/login/login.component.ts
@@ -28,7 +28,7 @@ export class LoginComponent {
 
       this.loginForm = this._fb.group({
         email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required]],
+        password: ['', [Validators.required, Validators.pattern(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ0-9,;/. ]*$/)]],
         rememberMe: ['']
       });
   }
