7fbcfcce9408b7bf9d9ade320d9b3ee139b73ab8
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-10-15 11:22:47 -0600

    Refactor tablero component HTML to fix typo in faculty name Update environment.prod.ts to use local endpoint for development

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    endpoint: "https://fce-backend-a3hfc3f2ejf3b2a9.canadacentral-01.azurewebsites.net/",
+    // endpoint: "https://fce-backend-a3hfc3f2ejf3b2a9.canadacentral-01.azurewebsites.net/",
+    endpoint: "http://localhost:5000/",
     apiUrl: "api",
   };
