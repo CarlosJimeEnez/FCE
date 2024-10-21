f8417153cbd85f8dab6a71c7cd1b098253855e40
CarlosJimeEnez <jjimenezpena7@gmail.com>
2024-10-21 11:11:58 -0600

    cambios de de endpoint

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    // endpoint: "https://fce-backend-a3hfc3f2ejf3b2a9.canadacentral-01.azurewebsites.net/",
-    endpoint: "http://fce-prueba.ece.buap.mx:5000/",
+    endpoint: "https://fce-backend-a3hfc3f2ejf3b2a9.canadacentral-01.azurewebsites.net/",
+    // endpoint: "http://fce-prueba.ece.buap.mx:5000/",
     apiUrl: "api",
   };
